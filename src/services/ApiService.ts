030e075169614d103f0d274f4b6097f7e47326b8
manuel-klocek <manuel.klocek@mail.schwarz>
2023-10-21 17:01:59 +0200

    updated pnpm added user notifications -> toasts and alerts completed login and register functions

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,3 +1,5 @@
+import ToastComponent from "@/components/ToastComponent.vue";
+
 export class ApiService {
     private token: String
 
@@ -5,7 +7,7 @@ export class ApiService {
         return this.token
     }
 
-    public async requestLogin(username: String, password: String) {
+    public async requestLogin(username: String, password: String): Promise<Boolean> {
         const response = await fetch('http://localhost:8080/api/login',  {
             method: 'POST',
             headers: {
@@ -13,12 +15,46 @@ export class ApiService {
             },
             body: JSON.stringify(
                 {
-
+                    username: username,
+                    password: password
                 }
             )
+        })
+
+        if(response.status == 401) {
+            console.log("username or password wrong")
+            return false
         }
-        )
+
+        const result = await response.json()
+        this.token = result["token"]
+        return true
     }
 
+    async requestRegister(name, pass, mail): Promise<boolean> {
+        const response = await fetch("http://localhost:8080/api/user", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    username: name,
+                    password: pass,
+                    mail: mail
+                }
+            )
+        })
 
+        if(response.status == 201) {
+            console.log("User created, proceed to log in")
+            return true
+        }
+        else if(response.status == 409)
+            console.log("User already exists! -> Make check user request on input!")
+        else
+            console.log("Bad Request with status code: " + response.status + " " + response.statusText)
+
+        return false
+    }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,95 @@
-<script lang="ts">
+<script lang="ts" setup>
 import { ApiService } from "@/services/ApiService";
+import { useRouter } from "vue-router";
+import { ref } from 'vue';
+import Swal from 'sweetalert2'
+import ToastComponent from "@/components/ToastComponent.vue";
 
-let api = new ApiService()
+const api = new ApiService()
+const router = useRouter()
+const useToast = ToastComponent.Toast
 
-export default {
-  data() {
-    return {
-      animate: false,
-      form: 'HEADLINE',
-      username: '',
-      password: '',
-      passwordConfirm: '',
-      mail: ''
-    };
-  },
-  methods: {
-    toggleHeadline() {
-      this.animate = !this.animate
-    },
-    toggleLogin() {
-      if(this.form === 'HEADLINE') {
-        this.form = 'LOGIN'
-        this.toggleHeadline()
-      } else if(this.form === 'LOGIN') {
-        this.form = 'HEADLINE'
-        this.toggleHeadline()
-      } else {
-        this.form = 'LOGIN'
-      }
-    },
-    toggleRegister() {
-      if(this.form === 'HEADLINE') {
-        this.form = 'REGISTER'
-        this.toggleHeadline()
-      } else if(this.form === 'REGISTER') {
-        this.form = 'HEADLINE'
-        this.toggleHeadline()
-      } else {
-        this.form = 'REGISTER'
-      }
-    },
-    login() {
-      api.requestLogin(this.username, this.password)
-    },
-    register() {
-      console.log("something")
-    }
-  },
-  mounted() {
-    document.title = "Welcome to QuizMe"
+const form = ref('HEADLINE')
+const animate = ref(false)
+const username = ref('')
+const password = ref('')
+const passwordConfirm = ref('')
+const mail = ref('')
+
+function toggleHeadline() {
+  animate.value = !animate.value
+}
+
+function toggleLogin() {
+  if(form.value === 'HEADLINE') {
+    form.value = 'LOGIN'
+    toggleHeadline()
+  } else if(form.value === 'LOGIN') {
+    form.value = 'HEADLINE'
+    toggleHeadline()
+  } else {
+    form.value = 'LOGIN'
   }
-};
+}
+
+function toggleRegister() {
+  if(form.value === 'HEADLINE') {
+    form.value = 'REGISTER'
+    toggleHeadline()
+  } else if(form.value === 'REGISTER') {
+    form.value = 'HEADLINE'
+    toggleHeadline()
+  } else {
+    form.value = 'REGISTER'
+  }
+}
+
+async function login() {
+  const success = await api.requestLogin(username.value, password.value)
+
+  if(success) {
+    router.push("/categories")
+    await useToast.fire({
+      icon: 'success',
+      title: 'Successfully logged in',
+      text: 'Welcome back ' + username.value + '!'
+    })
+  }
+  else {
+    await Swal.fire({
+      title: "Error",
+      text: "Username or password does not match",
+      icon: "error",
+      confirmButtonText: "Try again"
+    })
+  }
+}
+
+async function register() {
+  if(password.value != passwordConfirm.value) {
+    console.log("passwords must match!")
+    return
+  }
+
+  const success = await api.requestRegister(username.value, password.value, mail.value)
+
+  if(success) {
+    await useToast.fire({
+      icon: 'success',
+      title: 'User created successfully',
+      text: 'You are getting logged in...',
+      timer: 1500
+    })
+    await login()
+  } else {
+    await Swal.fire({
+      icon: 'error',
+      title: 'Failed',
+      text: 'User creation failed! Please try again'
+    })
+  }
+}
+
 </script>
 
 <template>
@@ -61,7 +100,7 @@ export default {
 
   <main>
     <!--  Logo  -->
-    <h1 class="headline" :class="{ 'headline-up': this.animate }">
+    <h1 class="headline" :class="{ 'headline-up': animate }">
       <span style="color: darkred">Q</span>
       <span style="color: darkblue">u</span>
       <span style="color: darkgoldenrod">i</span>
@@ -71,16 +110,16 @@ export default {
     </h1>
 
     <!-- Login and Register Button -->
-    <nav :class="{ 'nav-center': this.animate }">
+    <nav :class="{ 'nav-center': animate }">
       <a class="link" @click="toggleLogin">Login</a>
       <a class="link" @click="toggleRegister">Register</a>
     </nav>
 
     <!-- Login Form -->
-    <div v-if="this.form === 'LOGIN'">
-      <form @submit.prevent class="login-container" :class="{ 'open-form': this.animate }">
+    <div v-if="form === 'LOGIN'">
+      <form @submit.prevent class="login-container" :class="{ 'open-form': animate }">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" v-model="username">
+        <input type="text" placeholder="Enter Username" name="uname" v-model="username" autofocus>
 
         <label for="pass"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="pass" v-model="password">
@@ -90,10 +129,10 @@ export default {
     </div>
 
     <!-- Register Form -->
-    <div v-if="this.form === 'REGISTER'">
-      <form @submit.prevent class="register-container" :class="{ 'open-form': this.animate }">
+    <div v-else-if="form === 'REGISTER'">
+      <form @submit.prevent class="register-container" :class="{ 'open-form': animate }">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" v-model="username">
+        <input type="text" placeholder="Enter Username" name="uname" v-model="username" autofocus>
 
         <label for="mail"><b>Mail</b></label>
         <input type="email" placeholder="Enter Mail" name="mail" v-model="mail">
